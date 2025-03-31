0583418f708273efefa267cd0f868f850eb0a611
pokeyuke <augustogc134@gmail.com>
2025-03-31 21:15:57 +0200

    Final commit The project is now finished. I did some minor changes in the index file and the stylesheet. Most of the work was done in the script file, where I wrote the functions.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Calculator
 This project is a calculator made with html, css and javascript.
+
+This calculator can do only the four fundamental operations. Maybe in the future it will be improved.
+I'm open for any feedback.
--- script.js
+++ script.js
@@ -16,17 +16,56 @@ function divide(x, y){
 }
 
 function operate(operator, x, y){
-    return operator(x, y)
+    switch(operator){
+        case "+": return add(x, y);
+        case "-": return sub(x,y);
+        case "*": return multiply(x,y);
+        case "/": return divide(x, y);
+        default: return "Error";
+    }
 }
 
 const display = document.querySelector(".display")
 const btn = document.querySelectorAll("button")
+const clear = document.getElementById("clear");
+const equals = document.getElementById("equals")
 
+clear.addEventListener("click", () =>{
+    display.textContent = "";
+})
+
+clear.textContent = "C"
+
+function calculator() {
+    const expression = display.textContent;
+
+
+    const numbers = expression.split(/[+\-*/]/).map(num => parseFloat(num));
+    const operators = expression.split(/[0-9]+/).filter(char => char.match(/[+\-*/]/));
+
+
+    if (numbers.length === 2 && operators.length === 1) {
+        const x = numbers[0];
+        const y = numbers[1];
+        const operator = operators[0];  
+        const result = operate(operator, x, y);
+        display.textContent = result;
+    } else {
+        display.textContent = "Error";  
+    }
+}
 
 btn.forEach((button) => {
     button.addEventListener("click", () => {
-        display.textContent += button.textContent
-    })
-})
+        
+        if (button.textContent === "=") {
+            calculator();
+        } else if (button.textContent === "C") {
+            display.textContent = "";
+        } else {
+            display.textContent += button.textContent;
+        }
+    });
+});
 
 
